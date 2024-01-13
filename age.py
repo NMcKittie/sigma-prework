c4f73f9ce80caf2d5f725db2d9e52b3683ab05b5
Nathan Mckittrick <122108434+NMcKittie@users.noreply.github.com>
2024-01-13 16:08:54 +0000

    removing some comments and line breaks

--- age.py
+++ age.py
@@ -1,15 +1,11 @@
 
 from datetime import date, timedelta, datetime
 
-
-#then subtracting one if the current month/day precedes the birth month/day.
-
 def past_date(past_date, cur_date=date.today()):
     age = cur_date.year - past_date.year
     if cur_date.strftime('%d/%m') < past_date.strftime('%d/%m'):
         age -= 1
     return age
-
 
 def future_date(future_date, cur_date=date.today()):
     return cur_date.year - future_date.year
@@ -25,11 +21,9 @@ def get_user_date():
     datetim = datetime.strptime(user_date, '%d/%m/%Y')
     return datetim.date()
 
-
 def main():
     user_date = get_user_date()
     print("You are: {}".format(calculate_years(user_date)))
-
 
 if __name__ == "__main__":
     main()
