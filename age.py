1cd566c7e9d6291a45a3bd869c5e13843c9a77b5
NMcKittie <nathankit66@gmail.com>
2024-01-13 16:07:06 +0000

    Finished age calculator

--- age.py
+++ age.py
@@ -1,26 +1,34 @@
 
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+
+
+#then subtracting one if the current month/day precedes the birth month/day.
 
 def past_date(past_date, cur_date=date.today()):
-    return cur_date.year - past_date.year
+    age = cur_date.year - past_date.year
+    if cur_date.strftime('%d/%m') < past_date.strftime('%d/%m'):
+        age -= 1
+    return age
 
 
 def future_date(future_date, cur_date=date.today()):
-    return past_date.year - cur_date.year
+    return cur_date.year - future_date.year
 
-def if_past(user_date, cur_date=date.today()):
+def calculate_years(user_date, cur_date=date.today()):
     if user_date < cur_date:
-        return True
+        return past_date(user_date)
     else:
-        False
+        return future_date(user_date)
+
+def get_user_date():
+    user_date = input("Please enter a date in the format of dd/mm/yyyy: ")
+    datetim = datetime.strptime(user_date, '%d/%m/%Y')
+    return datetim.date()
 
 
 def main():
-    user_date = date(2033, 12, 30)
-    if if_past(user_date):
-        print(past_date(user_date))
-    else:
-        print(future_date(user_date))
+    user_date = get_user_date()
+    print("You are: {}".format(calculate_years(user_date)))
 
 
 if __name__ == "__main__":
